9461e970f1e4ac0c88a915207eee79883203582f
LoadingALIAS <alias@wsabilabs.com>
2025-04-07 01:20:32 -0400

    Shaking the Tree

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["esm", "cjs"],
   dts: true,
   clean: true,
   minify: true,
   target: "es2022",
-  splitting: false,
+  splitting: true,
+  treeshake: true,
+  sourcemap: true,
+  outDir: "dist",
+  external: ["node:fs/promises", "node:path"],
+  esbuildOptions(options) {
+    options.chunkNames = 'chunks/[name]-[hash]';
+    options.assetNames = 'assets/[name]-[hash]';
+  }
 });
